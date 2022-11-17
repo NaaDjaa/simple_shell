8c6f8c7eebed68ef67b4c6c64ecd2aa2efe54c6c
Naadjaa <barnor.esther@gmail.com>
2022-11-16 16:39:30 -0800

    1

--- shell.c
+++ shell.c
@@ -1,6 +1,15 @@
-#include <stdio.h>
+#include "main.h"
+/**
+ * swap_int - swaps values of two integers
+ * @a: first value of integer
+ * @b: second value of integer
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int swap_value = *a;
 
-int main() {
-	printf("Hello World!");
-	return 0;
+
+	*a = *b;
+	*b = swap_value;
 }
